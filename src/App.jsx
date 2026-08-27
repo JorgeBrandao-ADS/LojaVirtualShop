4bf7a44c232cf641ebb45f83526a0b65b3e2f09e
JorgeBrandao-ADS <jorgebrandaonote@outlook.com>
2026-08-27 14:55:40 -0300

    Carrinho com animações

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
 import Categorias from "./components/Categorias";
 import Vitrine from "./components/Vitrine";
-import Carrinho from "./components/Carrinho";
+import CarrinhoLateral from "./components/CarrinhoLateral";
 import Beneficios from "./components/Beneficios";
 import Footer from "./components/Footer";
 
@@ -397,7 +397,7 @@ function App() {
 
       {mostrarCarrinho && (
 
-        <Carrinho
+        <CarrinhoLateral
 
           carrinho={carrinho}
 
